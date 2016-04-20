aeb3a9ecd7ba9a9d71d5242f92f9368739377a36
ligi <ligi@ligi.de>
2016-04-20 12:59:22 +0200

    Initial scaffold for first first activity

---
+++ app/src/main/java/org/ligi/blueblab/FindPeerActivity.java
@@ -0,0 +1,15 @@
+package org.ligi.blueblab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.OnClick;
+import org.ligi.axt.AXT;
+
+public class FindPeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
--- app/src/main/java/org/ligi/blueblab/MainActivity.java
+++ app/src/main/java/org/ligi/blueblab/MainActivity.java
@@ -2,8 +2,16 @@ package org.ligi.blueblab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import butterknife.Bind;
+import butterknife.OnClick;
+import org.ligi.axt.AXT;
 
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.startFAB)
+    void onFABClick() {
+        AXT.at(this).startCommonIntent().activityFromClass(FindPeerActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
