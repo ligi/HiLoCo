b18548131f10d1f0b2fe27a9ecfc91a960594d2b
ligi <ligi@ligi.de>
2016-04-20 17:38:42 +0200

    Add home-as-up for chat

--- app/src/main/java/org/ligi/blueblab/ChatActivity.java
+++ app/src/main/java/org/ligi/blueblab/ChatActivity.java
@@ -2,6 +2,7 @@ package org.ligi.blueblab;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -9,6 +10,18 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        if (getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId()==android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
