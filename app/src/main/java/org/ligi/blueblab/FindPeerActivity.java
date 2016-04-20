416648dfc69161029fa5c3f1b94838a443303382
ligi <ligi@ligi.de>
2016-04-20 14:30:16 +0200

    Initial Avatar work

--- app/src/main/java/org/ligi/blueblab/FindPeerActivity.java
+++ app/src/main/java/org/ligi/blueblab/FindPeerActivity.java
@@ -7,17 +7,29 @@ import android.view.MenuItem;
 import android.view.View;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import java.util.Random;
 
 public class FindPeerActivity extends AppCompatActivity {
 
+    public static final int SIZE = 5;
     @Bind(R.id.gridLayout)
     SquareGridByWidthLayout gridLayout;
+
+    private class AvatarView {
+        private final View rootView;
+
+        private AvatarView(final View rootView) {
+            this.rootView = rootView;
+        }
+    }
+
+    private AvatarView myAvatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getSupportActionBar()!=null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
@@ -26,10 +38,28 @@ public class FindPeerActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         final LayoutInflater from = LayoutInflater.from(this);
+        final Random random = new Random();
+        final int MIDDLE = (SIZE / 2) ;
 
-        for (int i = 0; i < 25; i++) {
-            final View inflate = from.inflate(R.layout.avatar, gridLayout, true);
+
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                final View inflate = from.inflate(R.layout.avatar, gridLayout, false);
+
+                gridLayout.addView(inflate);
+                final AvatarView avatarView = new AvatarView(inflate);
+
+                final boolean b = random.nextInt() % 10 != 5;
+
+                if (x == MIDDLE && y == MIDDLE) {
+                    myAvatar = avatarView;
+                } else if (b) {
+                    avatarView.rootView.setVisibility(View.INVISIBLE);
+                }
+            }
         }
+
+        myAvatar.rootView.setVisibility(View.VISIBLE);
     }
 
 
